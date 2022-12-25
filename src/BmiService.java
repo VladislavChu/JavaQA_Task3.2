508a20447529aa02503d8b763b4c54d6de7e79ae
Vladislav Chudaev <117576235+VladislavChu@users.noreply.github.com>
2022-12-25 15:28:50 +0300

    reformat code

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double weightValue, double heightValue) {
-        double result = weightValue / (heightValue*heightValue);
+        double result = weightValue / (heightValue * heightValue);
         return result;
     }
 }
